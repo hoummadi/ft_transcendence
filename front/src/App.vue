b9f6ce1cc381682db733f819f7e59f5c07aba5bf
hoummadi <hamza.hoummadi.genie.info@gmail.com>
2022-11-06 18:33:58 +0100

    tailwind

---
+++ front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+  font-size: 2.5rem;
+  font-weight: bold;
+  position: relative;
+  overflow: hidden;
+}
+</style>
